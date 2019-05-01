1bf1898f2eaa1d6a50e540483e1b2d4c1aa6e4ce
Pantelis <tampantelis@gmail.com>
2019-05-01 10:34:36 +0300

    instaled yargs module and set up basic commands

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes =  require('./notes');
 
+// customize yargs version
 
-const command = process.argv[2];
+yargs.version('1.1.0');
 
-if (command === 'add') {
-  console.log('adding note');
-} else if (command === 'remove') {
-  console.log('removing note');
-}
+//Add command
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+    console.log('Adding a new note!');
+  }
+});
+
+//Remove command
+
+yargs.command({
+  command: 'remove',
+  describe: 'remove a note',
+  handler: function () {
+    console.log('Removing the note!');
+  }
+});
+
+//List command
+
+yargs.command({
+  command: 'list',
+  describe: 'list all notes',
+  handler: function () {
+    console.log('Listin all notes');
+  }
+});
+
+//Read commmand
+
+yargs.command({
+  command: 'read',
+  describe: 'read a note',
+  handler: function () {
+    console.log('Reading the note!');
+  }
+});
+
+
+
+console.log(yargs.argv);
