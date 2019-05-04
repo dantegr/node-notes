fb9de3078843181c1699c759b50330ceca150d08
Pantelis <tampantelis@gmail.com>
2019-05-04 20:58:43 +0300

    added debugger function

--- notes.js
+++ notes.js
@@ -10,7 +10,9 @@ const addNote = (title, body) => {
   const notes = loadNotes();
 
   const duplicateNote = notes.find((note) => note.title === title);
-
+  
+  debugger;
+  
   if (!duplicateNote){
     notes.push({
       title: title,
